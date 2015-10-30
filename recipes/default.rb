c07ed43efd0b0c052f7de0f5b80b17a7637317fa
Daniel Klopp <dklopp@infoblox.com>
2015-10-29 20:18:17 -0700

    Adding more deps for rails and ruby installation

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,6 +12,12 @@ include_recipe 'build-essential'
 
 package [ "git", "libsqlite3-dev", "nodejs" ]
 
+%w(git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev 
+	libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev
+	python-software-properties libffi-dev).each do |pack|
+	package pack
+end
+
 directory "/srv/myapp" do
 	owner 'root'
 	group 'root'
